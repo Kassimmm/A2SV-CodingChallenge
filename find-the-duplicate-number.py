9dafe2eea0d2be470729bedfb3f5417c91291d73
Kassimmm <102239561+Kassimmm@users.noreply.github.com>
2023-09-18 13:05:09 +0000

    Update find-the-duplicate-number.py

--- find-the-duplicate-number.py
+++ find-the-duplicate-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int: 
-        hashmap = defaultdict(int)
-        for i in range(len(nums)):
-            hashmap[nums[i]] += 1
-            if hashmap[nums[i]] > 1:
-                return nums[i] 
+        nums.sort()
+
+        for i in nums:
+            if (bisect.bisect_right(nums, i)-1) != (bisect.bisect_left(nums, i)):
+                return i
