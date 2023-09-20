11575daf2b8bb0c34f8669a2ec58ba2e36a1c1d1
Kassimmm <102239561+Kassimmm@users.noreply.github.com>
2023-09-20 17:19:39 +0000

    Create power-of-three.py

---
+++ power-of-three.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        def isPower(num):
+            if num == 1:
+                return True
+            elif num < 1:
+                return False
+            return isPower(num / 3)
+
+        return isPower(n)
