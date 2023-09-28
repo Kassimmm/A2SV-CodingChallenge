b2323ae7fb80406c3ee51d12415082534410f597
Kassimmm <102239561+Kassimmm@users.noreply.github.com>
2023-09-28 11:10:35 +0000

    Update and rename smallest-even-multiple to smallest-even-multiple.py

--- smallest-even-multiple.py
+++ smallest-even-multiple.py
@@ -1,10 +1,16 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 0:
+            return n
+        else:
+            return n * 2
+
+"""
 class Solution(object):
     def smallestEvenMultiple(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
         last_factor = (n * 2) + 1
         for num in range(1, last_factor):
             if num % 2 == 0 and num % n == 0:
                 return num
+This is the brute force approach
+"""
