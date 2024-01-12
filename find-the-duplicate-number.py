c35023e1be88dbad4533bcb5105d9d5d834f505f
Kassimmm <102239561+Kassimmm@users.noreply.github.com>
2024-01-12 00:37:38 +0000

    Update find-the-duplicate-number.py

--- find-the-duplicate-number.py
+++ find-the-duplicate-number.py
@@ -1,7 +1,22 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int: 
-        nums.sort()
+        n = len(nums)
+        i = 0
 
-        for i in nums:
-            if (bisect.bisect_right(nums, i)-1) != (bisect.bisect_left(nums, i)):
-                return i
+        while i < n:
+            if 1 <= nums[i] <= n and nums[i] != i+1 and nums[i] != nums[nums[i]-1]:
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1] 
+            else:
+                i += 1
+
+        return next((nums[i] for i in range(len(nums)) if nums[i] != i+1), 0)   ## using cyclic sort
+
+
+
+# class Solution:
+#     def findDuplicate(self, nums: List[int]) -> int: 
+#         nums.sort()
+
+#         for i in nums:
+#             if (bisect.bisect_right(nums, i)-1) != (bisect.bisect_left(nums, i)):
+#                 return i
