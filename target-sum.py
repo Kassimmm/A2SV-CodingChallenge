10a680c4336faa1da7a8ceaf8dfef1690d4979da
Kassimmm <102239561+Kassimmm@users.noreply.github.com>
2024-02-22 17:36:20 +0000

    Update target-sum.py

--- target-sum.py
+++ target-sum.py
@@ -1,16 +1,13 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        memo = {}
+        prev = {0:1}
 
-        def dp(i, curr):
-            if i == len(nums):
-                return int(curr == target)
+        for num in nums:
+            curr = defaultdict(int)
+            for subp in prev:
+                curr[subp + num] += prev[subp]
+                curr[subp - num] += prev[subp]
+            
+            prev = curr
 
-            if (i, curr) not in memo:
-                a = dp(i+1, -nums[i]+curr)
-                b = dp(i+1, nums[i]+curr)
-                memo[(i, curr)] = a+b
-
-            return memo[(i, curr)]
-
-        return dp(0,0)
+        return prev[target] 
