a6db90daf1634e02e59ca758fb627177737122dd
Kassimmm <102239561+Kassimmm@users.noreply.github.com>
2024-03-05 16:59:37 +0000

    Update find-if-path-exists-in-graph.py

--- find-if-path-exists-in-graph.py
+++ find-if-path-exists-in-graph.py
@@ -1,19 +1,39 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = [i for i in range(size)] 
+        self.size = [1] * size
+
+
+    def find(self, node):
+        if node == self.parent[node]:
+            return node
+        
+        self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+        
+    
+    def union(self, u, v):
+        root1 = self.find(u)
+        root2 = self.find(v)
+        
+        if root1 != root2:
+            if self.size[root1] > self.size[root2]:
+                self.parent[root2] = root1
+                self.size[root1] += self.size[root2]
+            else:
+                self.parent[root1] = root2
+                self.size[root2] += self.size[root1]
+
+
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = defaultdict(list)
-        for i in edges:
-            graph[i[0]].append(i[1])
-            graph[i[1]].append(i[0])
+        graph = UnionFind(n)
 
-        def dfs(vertex, visited):
-            if vertex == destination:
-                return True
-            if vertex in visited:
-                return False
-            visited.add(vertex)
-            for neighbour in graph[vertex]:
-                temp = dfs(neighbour, visited)
-                if temp:
-                    return True
-            return False
-        return dfs(source, set())
+        for edge in edges:
+            graph.union(edge[0], edge[-1])
+
+        target = graph.find(source)
+        
+        return graph.find(source) == graph.find(destination)
+        
