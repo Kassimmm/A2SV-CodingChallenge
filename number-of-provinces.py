046fdf41e6c9e7dfc51b050622c778317383a51a
Kassimmm <102239561+Kassimmm@users.noreply.github.com>
2024-03-05 16:56:09 +0000

    Update number-of-provinces.py

--- number-of-provinces.py
+++ number-of-provinces.py
@@ -1,24 +1,47 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = [i for i in range(size)]
+        self.size = [1] * size
+
+
+    def find(self, node):
+        if node == self.parent[node]:
+            return node
+        
+        self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+        
+    
+    def union(self, u, v):
+        root1 = self.find(u)
+        root2 = self.find(v)
+        
+        if root1 != root2:
+            if self.size[root1] > self.size[root2]:
+                self.parent[root2] = root1
+                self.size[root1] += self.size[root2]
+            else:
+                self.parent[root1] = root2
+                self.size[root2] += self.size[root1]
+
+
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        graph = defaultdict(list)
+        graph = UnionFind(len(isConnected))
+
         for i in range(len(isConnected)):
-            for j in range(len(isConnected[0])):
-                if isConnected[i][j] == 1:
-                    graph[i].append(j)
-                
+            for j in range(len(isConnected)):
+                if isConnected[i][j]:
+                    graph.union(i, j)
+        
+        res = 0
+        visited = set()
 
-        visited, count = set(), 0
-        def dfs(node,visited):
-            if node in visited:
-                return
+        for i in range(len(isConnected)):
+            if graph.find(i) not in visited:
+                visited.add(graph.find(i))
+                res += 1
 
-            visited.add(node)
-            for v in graph[node]:
-                if v not in visited:
-                    dfs(v,visited)      
-
-        for node in graph:
-            if node not in visited:
-                dfs(node,visited)
-                count += 1
-        return count 
+        print(visited)
+        return res       
