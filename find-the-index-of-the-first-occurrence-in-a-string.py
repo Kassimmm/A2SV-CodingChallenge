f054461a5761838fe797ab660a85fa0c50929069
Kassimmm <102239561+Kassimmm@users.noreply.github.com>
2024-03-21 14:36:38 +0000

    Create find-the-index-of-the-first-occurrence-in-a-string.py

---
+++ find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        def hash(string):
+            res = 0
+            for i in range(len(string)):
+                res += (ord(string[i]) - 96) * (26 ** (n-i-1))
+            return res    
+
+        def add(char, code):
+            code *= 26
+            code += (ord(char)-96)
+            return code
+
+        def remove(char, code):
+            code -= (26 ** (n-1)) * (ord(char) - 96)
+            return code
+
+        m, n = len(haystack), len(needle)
+        temp = hash(haystack[:n])
+        target = hash(needle)
+        if temp == target:
+            return 0
+
+
+        for i in range(n, m):
+            temp = remove(haystack[i-n], temp)
+            temp = add(haystack[i], temp)
+
+            if temp == target:
+                return (i-n+1)
+                
+        return -1
