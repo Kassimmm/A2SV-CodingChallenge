5cb96b27ac94a788ecf8054ee4d2b459905fc666
Kassimmm <102239561+Kassimmm@users.noreply.github.com>
2024-03-22 16:38:42 +0000

    Create longest-happy-prefix.py

---
+++ longest-happy-prefix.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestPrefix(self, s: str) -> str:
+        base = 31
+        m = len(s)
+        mod = ((10 ** 9)+7)
+        res = ""
+        hash1 = (ord(s[0]) - 96) % mod
+        hash2 = (ord(s[-1]) - 96) % mod 
+
+        for i in range(1, len(s)-1):
+            if hash1 == hash2: res = s[:i]
+
+            hash1 *= 31
+            hash1 += (ord(s[i])-96)
+            hash1 %= mod
+
+            hash2 += ((ord(s[m-i-1])-96) * base)
+            hash2 %= mod
+            base *= 31
+            base %= mod
+
+        if hash1 == hash2: res = s[:m-1]
+
+        return res
+        
